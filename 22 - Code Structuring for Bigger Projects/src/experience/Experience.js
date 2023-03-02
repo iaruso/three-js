ab7b1a6876d5eeaafdff4ecc1cb1b02c8c15f6fc
iaruso <90287515+iaruso@users.noreply.github.com>
2023-03-02 22:23:36 +0000

    22 - Code Structuring for Bigger Projects (Part 2)

--- 22 - Code Structuring for Bigger Projects/src/experience/Experience.js
+++ 22 - Code Structuring for Bigger Projects/src/experience/Experience.js
@@ -2,6 +2,11 @@ import * as THREE from 'three'
 import Sizes from './utils/Sizes.js'
 import Time from './utils/Time.js'
 import Camera from './Camera.js'
+import Renderer from './Renderer.js'
+import World from './world/World.js'
+import Debug from './utils/Debug.js'
+import Resources from './utils/Resources.js'
+import sources from './sources.js'
 
 let instance  = null
 
@@ -15,11 +20,14 @@ export default class Experience {
         window.experience = this
 
         this.canvas = canvas
-
+        this.debug = new Debug()
         this.sizes = new Sizes()
         this.time = new Time()
         this.scene = new THREE.Scene()
         this.camera = new Camera()
+        this.resources = new Resources(sources)
+        this.renderer = new Renderer()
+        this.world = new World()
 
         this.sizes.on('resize', () => {
             this.resize()
@@ -32,9 +40,35 @@ export default class Experience {
 
     resize() {
         this.camera.resize()
+        this.renderer.resize()
     }
 
     update() {
         this.camera.update()
+        this.world.update()
+        this.renderer.update()
+    }
+
+    destroy() {
+        this.sizes.off('resize')
+        this.time.off('tick')
+
+        this.scene.traverse((child) => {
+            if(child instanceof THREE.Mesh) {
+                child.geometry.dispose()
+                for(const key in child.material)  {
+                    const value = child.material[key]
+                    if(value && typeof value.dispose === 'function') {
+                        value.dispose()
+                    }
+                }
+            }
+        })
+
+        this.camera.controls.dispose()
+        this.renderer.instance.dispose()
+
+        if(this.debug.active)
+        this.debug.ui.destroy()
     }
 }
---
+++ 22 - Code Structuring for Bigger Projects/src/experience/utils/Debug.js
@@ -0,0 +1,10 @@
+import * as dat from 'dat.gui'
+
+export default class Debug {
+    constructor() {
+        this.active = window.location.hash === '#debug'
+        if(this.active) {
+            this.ui = new dat.GUI()
+        }
+    }
+}
