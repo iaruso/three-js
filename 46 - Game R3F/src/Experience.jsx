23d251f29f780b4ed93c9d039b8d8b00c2cd04b9
iaruso <iaruso.me@gmail.com>
2023-05-01 03:51:10 +0100

    46 - Game R3F (Part 2)

--- 46 - Game R3F/src/Experience.jsx
+++ 46 - Game R3F/src/Experience.jsx
@@ -2,15 +2,23 @@ import { OrbitControls } from '@react-three/drei'
 import { Physics, Debug } from '@react-three/rapier'
 import Lights from './Lights.jsx'
 import { Level } from './Level.jsx'
+import Player from './Player.jsx'
+import useGame from './stores/useGame.jsx'
+import Effects from './Effects.jsx'
 
 export default function Experience() {
-    return <>
+    const blocksCount = useGame((state) => state.blocksCount)
+    const blocksSeed = useGame(state => state.blocksSeed)
 
-        <OrbitControls makeDefault />
-        <Physics>
-            <Debug/>
+    return <>
+        <color args={ [ '#252731' ] } attach="background" />
+        {/* <OrbitControls makeDefault /> */}
+        <Physics timeStep="vary">
+            {/* <Debug/> */}
             <Lights />
-            <Level />
+            <Level count={ blocksCount } seed={ blocksSeed } />
+            <Player />
         </Physics>
+        <Effects />
     </>
 }
--- 46 - Game R3F/src/Level.jsx
+++ 46 - Game R3F/src/Level.jsx
@@ -1,20 +1,33 @@
 import { useMemo, useState, useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
-import { RigidBody } from '@react-three/rapier'
-import { useGLTF } from '@react-three/drei'
-THREE.ColorManagement.legacyMode = true
+import { CuboidCollider, RigidBody } from '@react-three/rapier'
+import { Float, Text, useGLTF } from '@react-three/drei'
 
 const boxGeometry = new THREE.BoxGeometry( 1, 1, 1 )
 
-const floor1Material = new THREE.MeshStandardMaterial({ color: 'limegreen' })
-const floor2Material = new THREE.MeshStandardMaterial({ color: 'greenyellow' })
-const obstacleMaterial = new THREE.MeshStandardMaterial({ color: 'orangered' })
-const wallMaterial = new THREE.MeshStandardMaterial({ color: 'slategrey' })
+const floor1Material = new THREE.MeshStandardMaterial({ color: '#111111', metalness: 0, roughness: 0 })
+const floor2Material = new THREE.MeshStandardMaterial({ color: '#222222', metalness: 0, roughness: 0 })
+const obstacleMaterial = new THREE.MeshStandardMaterial({ color: '#ff0000', metalness: 0, roughness: 1 })
+const wallMaterial = new THREE.MeshStandardMaterial({ color: '#887777', metalness: 0, roughness: 0 })
 
 export function BlockStart({ position = [0, 0, 0]}) {
     return <group position={position}>
-        <mesh geometry={ boxGeometry} material={ floor1Material } position={ [ 0, -0.1, 0 ] } scale={ [ 4, 0.2, 4 ] }>
+        <Float floatIntensity={ 0.25 } rotationIntensity={ 0.25 }>
+        <Text
+            font="./bebas-neue-v9-latin-regular.woff"
+            scale={ 0.5 }
+            maxWidth={ 0.25 }
+            lineHeight={ 0.75 }
+            textAlign="right"
+            position={ [ 0.75, 0.65, 0 ] }
+            rotation-y={ - 0.25 }
+        >
+            Marble Race
+            <meshBasicMaterial toneMapped={ false } />
+        </Text>
+        </Float>
+        <mesh geometry={ boxGeometry} material={ floor1Material } position={ [ 0, -0.1, 0 ] } scale={ [ 4, 0.2, 4 ] } receiveShadow>
             {/* <boxGeometry args={[ 4, 0.2, 4 ]} receiveShadow/> */}
             {/* <meshStandardMaterial color="limegreen"/> */}
         </mesh>
@@ -83,14 +96,54 @@ export function BlockEnd({ position = [ 0, 0, 0 ] }) {
     })
 
     return <group position={ position }>
+        <Text
+            font="./bebas-neue-v9-latin-regular.woff"
+            scale={ 2 }
+            position={ [ 0, 2.25, 2 ] }
+        >
+            FINISH
+            <meshBasicMaterial toneMapped={ false } />
+        </Text>
         <mesh geometry={ boxGeometry } material={ floor1Material } position={ [ 0, 0, 0 ] } scale={ [ 4, 0.2, 4 ] } receiveShadow />
         <RigidBody type="fixed" colliders="hull" position={ [ 0, 0.25, 0 ] } restitution={ 0.2 } friction={ 0 } >
             <primitive object={ hamburger.scene } scale={ 0.2 } />
         </RigidBody>
     </group>
 }
+function Bounds({ length = 1 }) {
+    return <>
+        <RigidBody type="fixed" restitution={ 0.2 } friction={ 0 }>
+            <mesh
+                position={ [ 2.15, 0.75, - (length * 2) + 2 ] }
+                geometry={ boxGeometry }
+                material={ wallMaterial }
+                scale={ [ 0.3, 1.5, 4 * length ] }
+                castShadow
+            />
+            <mesh
+                position={ [ - 2.15, 0.75, - (length * 2) + 2 ] }
+                geometry={ boxGeometry }
+                material={ wallMaterial }
+                scale={ [ 0.3, 1.5, 4 * length ] }
+                receiveShadow
+            />
+            <mesh
+                position={ [ 0, 0.75, - (length * 4) + 2] }
+                geometry={ boxGeometry }
+                material={ wallMaterial }
+                scale={ [ 4, 1.5, 0.3 ] }
+                receiveShadow
+            />
+            <CuboidCollider args={ [ 2, 0.1, 2 * length ] }
+                position={ [ 0, -0.1, - (length * 2) + 2 ] }
+                restitution={ 0.2 }
+                friction={ 1 }
+            />
+        </RigidBody>
+    </>
+}
 
-export function Level({ count = 5, types = [ BlockSpinner, BlockAxe, BlockLimbo ] }) {
+export function Level({ count = 5, types = [ BlockSpinner, BlockAxe, BlockLimbo ], seed = 0}) {
     const blocks = useMemo(() => {
         const blocks = []
 
@@ -101,7 +154,7 @@ export function Level({ count = 5, types = [ BlockSpinner, BlockAxe, BlockLimbo 
         }
 
         return blocks
-    }, [ count, types ])
+    }, [ count, types, seed  ])
 
     return <>
         <BlockStart position={ [ 0, 0, 0 ] } />
@@ -109,5 +162,7 @@ export function Level({ count = 5, types = [ BlockSpinner, BlockAxe, BlockLimbo 
         { blocks.map((Block, index) => <Block key={ index } position={ [ 0, 0, - (index + 1) * 4 ] } />) }
 
         <BlockEnd position={ [ 0, 0, - (count + 1) * 4 ] } />
+
+        <Bounds length={ count + 2 } />
     </>
 }
